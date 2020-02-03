9eb2a778fba3f1b4e060ecd751572bf4dd5a0e38
chohanjoo <johanjoo@naver.com>
2020-02-03 11:21:12 +0900

    Finish V3

--- src/main/java/microservices/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/microservices/multiplication/service/MultiplicationServiceImpl.java
@@ -2,17 +2,31 @@ package microservices.multiplication.service;
 
 import microservices.multiplication.domain.Multiplication;
 import microservices.multiplication.domain.MultiplicationResultAttempt;
+import microservices.multiplication.domain.User;
+import microservices.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.multiplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 class MultiplicationServiceImpl implements MultiplicationService {
 
   private RandomGeneratorService randomGeneratorService;
+  private MultiplicationResultAttemptRepository attemptRepository;
+  private UserRepository userRepository;
 
   @Autowired
-  public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService) {
+  public MultiplicationServiceImpl(final RandomGeneratorService randomGeneratorService,
+                                   final MultiplicationResultAttemptRepository attemptRepository,
+                                   final UserRepository userRepository) {
     this.randomGeneratorService = randomGeneratorService;
+    this.attemptRepository = attemptRepository;
+    this.userRepository = userRepository;
   }
 
   @Override
@@ -22,10 +36,35 @@ class MultiplicationServiceImpl implements MultiplicationService {
     return new Multiplication(factorA, factorB);
   }
 
+  @Transactional
   @Override
-  public boolean checkAttempt(final MultiplicationResultAttempt resultAttempt) {
-    return resultAttempt.getResultAttempt() ==
-            resultAttempt.getMultiplication().getFactorA() *
-                    resultAttempt.getMultiplication().getFactorB();
+  public boolean checkAttempt(final MultiplicationResultAttempt attempt) {
+    // 해당 닉네임의 사용자가 존재하는지 확인
+    Optional<User> user = userRepository.findByAlias(attempt.getUser().getAlias());
+
+    // 조작된 답안을 방지
+    Assert.isTrue(!attempt.isCorrect(), "채점한 상태로 보낼 수 없습니다!!");
+
+    // 답안을 채점
+    boolean isCorrect = attempt.getResultAttempt() ==
+            attempt.getMultiplication().getFactorA() *
+                    attempt.getMultiplication().getFactorB();
+
+    MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(
+            user.orElse(attempt.getUser()),
+            attempt.getMultiplication(),
+            attempt.getResultAttempt(),
+            isCorrect
+    );
+
+    // 답안을 저장
+    attemptRepository.save(checkedAttempt);
+
+    return isCorrect;
+  }
+
+  @Override
+  public List<MultiplicationResultAttempt> getStatsForUser(String userAlias) {
+    return attemptRepository.findTop5ByUserAliasOrderByIdDesc(userAlias);
   }
 }
