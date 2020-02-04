11c86a38dee938d1d28619489a27800670cf69bb
chohanjoo <johanjoo@naver.com>
2020-02-04 18:50:46 +0900

    Add microservies gamification

---
+++ gamification/src/main/java/microservices/gamification/controller/LeaderBoardController.java
@@ -0,0 +1,28 @@
+package microservices.gamification.controller;
+
+import microservices.gamification.domain.LeaderBoardRow;
+import microservices.gamification.service.LeaderBoardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Gamification 리더보드 서비스의 REST API
+ */
+@RestController
+@RequestMapping("/leaders")
+class LeaderBoardController {
+
+  private final LeaderBoardService leaderBoardService;
+
+  public LeaderBoardController(final LeaderBoardService leaderBoardService) {
+    this.leaderBoardService = leaderBoardService;
+  }
+
+  @GetMapping
+  public List<LeaderBoardRow> getLeaderBoard() {
+    return leaderBoardService.getCurrentLeaderBoard();
+  }
+}
